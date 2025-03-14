5bff01296f541814cf6f20fd0d4a932921af1d2e
中宮 ちひろ <ciel_katana@outlook.com>
2025-03-14 17:31:27 +0800

    add sst format

--- remdb/src/format.rs
+++ remdb/src/format.rs
@@ -6,10 +6,16 @@ pub const VLOF_FILE_SUFFIX: &str = ".vlog";
 
 pub const WAL_FILE_SUFFIX: &str = ".wal";
 
+pub const SST_FILE_SUFFIX: &str = ".sst";
+
 pub fn vlog_format_path(dir: impl AsRef<Path>, fid: u32) -> PathBuf {
     dir.as_ref().join(format!("{:09}{VLOF_FILE_SUFFIX}", fid))
 }
 
 pub fn wal_format_path(dir: impl AsRef<Path>, fid: u32) -> PathBuf {
     dir.as_ref().join(format!("{:09}{WAL_FILE_SUFFIX}", fid))
+}
+
+pub fn sst_format_path(dir: impl AsRef<Path>, fid: u32) -> PathBuf {
+    dir.as_ref().join(format!("{:09}{SST_FILE_SUFFIX}", fid))
 }
