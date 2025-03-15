1653de8730a09cb6d25a2b965ce09cc17ab9be57
中宮 ちひろ <ciel_katana@outlook.com>
2025-03-15 21:07:06 +0800

    add table

--- remdb/src/table/meta_block.rs
+++ remdb/src/table/meta_block.rs
@@ -1,5 +1,7 @@
 #![allow(unused)]
 
+use crate::format::key::Seq;
+
 pub const MAGIC_NUMBER: u64 = 0x1145141919810;
 
 #[derive(Debug, Clone)]
@@ -8,6 +10,7 @@ pub struct MetaBlock {
     filters_start: u64,
     offsets_start: u64,
     block_count: u64,
+    max_seq: Seq,
     compress_type: u8,
     // crc: u32,
     // magic: u64,
--- remdb/src/table/mod.rs
+++ remdb/src/table/mod.rs
@@ -1,4 +1,16 @@
+use bloom::Bloom;
+use meta_block::MetaBlock;
+
+use crate::fs::File;
+
 pub mod block;
 pub mod bloom;
 pub mod meta_block;
 pub mod table_builder;
+
+pub struct Table {
+    file: File,
+
+    bloom: Bloom,
+    table_meta: MetaBlock,
+}
