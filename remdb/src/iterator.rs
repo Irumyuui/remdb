f272c6d824a34d0764ba5ccbb497bbcc8539aa5b
中宮 ちひろ <ciel_katana@outlook.com>
2025-03-17 20:45:13 +0800

    add more merge iter

--- remdb/src/iterator.rs
+++ remdb/src/iterator.rs
@@ -11,7 +11,7 @@ use crate::{
 };
 
 pub trait Iter: Send + Sync {
-    type KeyType<'a>: PartialEq + Eq + PartialOrd + Ord
+    type KeyType<'a>: PartialEq + Eq + PartialOrd + Ord + Send + Sync
     where
         Self: 'a;
 
@@ -166,6 +166,98 @@ where
     }
 }
 
+// A first
+pub struct TwoMergeIter<A, B>
+where
+    A: Iter,
+    B: Iter,
+{
+    a: A,
+    b: B,
+    use_a_flag: bool,
+}
+
+impl<A, B> TwoMergeIter<A, B>
+where
+    A: Iter + 'static,
+    B: Iter + 'static + for<'a> Iter<KeyType<'a> = A::KeyType<'a>>,
+{
+    async fn update_use_flag(&mut self) {
+        self.use_a_flag = if !self.a.is_valid().await {
+            false
+        } else if !self.b.is_valid().await {
+            true
+        } else {
+            self.a.key().await < self.b.key().await
+        };
+
+        eprintln!("use_a res: {}", self.use_a_flag);
+    }
+
+    async fn skip_b(&mut self) -> Result<()> {
+        while self.a.is_valid().await
+            && self.b.is_valid().await
+            && self.a.key().await == self.b.key().await
+        {
+            eprintln!("skip b!");
+            self.b.next().await?;
+        }
+        Ok(())
+    }
+
+    pub async fn new(a: A, b: B) -> Result<Self> {
+        let mut this = Self {
+            a,
+            b,
+            use_a_flag: true,
+        };
+        this.skip_b().await?;
+        this.update_use_flag().await;
+        Ok(this)
+    }
+}
+
+macro_rules! with_current_iter {
+    ($self:ident, $method:ident($($args:expr),*)) => {
+        if $self.use_a_flag {
+            eprintln!("use a");
+            $self.a.$method($($args),*).await
+        } else {
+            eprintln!("use b");
+            $self.b.$method($($args),*).await
+        }
+    };
+}
+
+impl<A, B> Iter for TwoMergeIter<A, B>
+where
+    A: Iter + 'static,
+    B: Iter + 'static + for<'a> Iter<KeyType<'a> = A::KeyType<'a>>,
+{
+    type KeyType<'a> = A::KeyType<'a>;
+
+    async fn is_valid(&self) -> bool {
+        with_current_iter!(self, is_valid())
+    }
+
+    async fn key(&self) -> Self::KeyType<'_> {
+        with_current_iter!(self, key())
+    }
+
+    async fn value(&self) -> Value {
+        with_current_iter!(self, value())
+    }
+
+    async fn next(&mut self) -> Result<()> {
+        with_current_iter!(self, next())?;
+
+        self.skip_b().await?;
+        self.update_use_flag().await;
+
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::{fmt::Debug, sync::Arc};
@@ -294,5 +386,138 @@ mod tests {
         for (exp, atc) in expected.iter().zip(actual.iter()) {
             assert_eq!(exp, atc);
         }
+    }
+
+    #[tokio::test]
+    async fn test_two_merge_iter() {
+        let mut data_a = vec![];
+        for i in 0..5 {
+            let key = Bytes::from(format!("key-{:05}", i));
+            let ts = i as u64;
+            let key = KeyBytes::new(key, ts);
+            let value = Bytes::from(format!("value-a-{:05}", i));
+            data_a.push((key, value));
+        }
+
+        let mut data_b = vec![];
+        for i in 2..7 {
+            let key = Bytes::from(format!("key-{:05}", i));
+            let ts = i as u64;
+            let key = KeyBytes::new(key, ts);
+            let value = Bytes::from(format!("value-b-{:05}", i));
+            data_b.push((key, value));
+        }
+
+        let mock_data_a = MockData::new(data_a);
+        let mock_data_b = MockData::new(data_b);
+        let iter_a = mock_data_a.iter();
+        let iter_b = mock_data_b.iter();
+
+        let mut merge_iter = super::TwoMergeIter::new(iter_a, iter_b).await.unwrap();
+
+        let mut actual = vec![];
+        while merge_iter.is_valid().await {
+            actual.push((
+                merge_iter.key().await.into_key_bytes(),
+                Bytes::copy_from_slice(&merge_iter.value().await.value_or_ptr),
+            ));
+            merge_iter.next().await.unwrap();
+        }
+
+        let mut expected = vec![];
+        let mut keys_from_a = mock_data_a
+            .items
+            .iter()
+            .map(|(k, _)| k.clone())
+            .collect::<Vec<_>>();
+
+        for (key, value) in mock_data_a.items.iter() {
+            expected.push((key.clone(), value.clone()));
+        }
+
+        for (key, value) in mock_data_b.items.iter() {
+            if !keys_from_a.contains(key) {
+                expected.push((key.clone(), value.clone()));
+            }
+        }
+
+        expected.sort_by(|a, b| a.0.cmp(&b.0));
+
+        assert_eq!(actual.len(), expected.len());
+        for (exp, act) in expected.iter().zip(actual.iter()) {
+            assert_eq!(exp, act);
+        }
+    }
+
+    #[tokio::test]
+    async fn test_two_merge_iter_a_is_empty() -> anyhow::Result<()> {
+        let mock_data_a = MockData::new(vec![]);
+        let mut data_b = vec![];
+        for i in 0..3 {
+            let key = Bytes::from(format!("key-{:05}", i));
+            let ts = i as u64;
+            let key = KeyBytes::new(key, ts);
+            let value = Bytes::from(format!("value-b-{:05}", i));
+            data_b.push((key, value));
+        }
+        let mock_data_b = MockData::new(data_b);
+
+        let iter_a = mock_data_a.iter();
+        let iter_b = mock_data_b.iter();
+
+        let mut merge_iter = super::TwoMergeIter::new(iter_a, iter_b).await?;
+
+        let mut actual = vec![];
+        while merge_iter.is_valid().await {
+            actual.push((
+                merge_iter.key().await.into_key_bytes(),
+                Bytes::copy_from_slice(&merge_iter.value().await.value_or_ptr),
+            ));
+            merge_iter.next().await?;
+        }
+
+        assert_eq!(actual.len(), mock_data_b.items.len());
+        for ((key_a, val_a), (key_b, val_b)) in actual.iter().zip(mock_data_b.items.iter()) {
+            assert_eq!(key_a, key_b);
+            assert_eq!(val_a, val_b);
+        }
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_two_merge_iter_b_is_empty() -> anyhow::Result<()> {
+        let mut data_a = vec![];
+        for i in 0..3 {
+            let key = Bytes::from(format!("key-{:05}", i));
+            let ts = i as u64;
+            let key = KeyBytes::new(key, ts);
+            let value = Bytes::from(format!("value-a-{:05}", i));
+            data_a.push((key, value));
+        }
+        let mock_data_a = MockData::new(data_a);
+        let mock_data_b = MockData::new(vec![]);
+
+        let iter_a = mock_data_a.iter();
+        let iter_b = mock_data_b.iter();
+
+        let mut merge_iter = super::TwoMergeIter::new(iter_a, iter_b).await?;
+
+        let mut actual = vec![];
+        while merge_iter.is_valid().await {
+            actual.push((
+                merge_iter.key().await.into_key_bytes(),
+                Bytes::copy_from_slice(&merge_iter.value().await.value_or_ptr),
+            ));
+            merge_iter.next().await?;
+        }
+
+        assert_eq!(actual.len(), mock_data_a.items.len());
+        for ((key_a, val_a), (key_b, val_b)) in actual.iter().zip(mock_data_a.items.iter()) {
+            assert_eq!(key_a, key_b);
+            assert_eq!(val_a, val_b);
+        }
+
+        Ok(())
     }
 }
